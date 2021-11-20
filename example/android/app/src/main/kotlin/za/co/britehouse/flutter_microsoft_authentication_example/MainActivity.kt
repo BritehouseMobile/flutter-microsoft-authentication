d59f9c94aabe5b1cd6e2a3c7ae81e1751e2cc159
Rob <74053268+robaburgess@users.noreply.github.com>
2021-11-20 16:29:12 +0000

    io.flutter.embedding.android.FlutterActivity

--- example/android/app/src/main/kotlin/za/co/britehouse/flutter_microsoft_authentication_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/za/co/britehouse/flutter_microsoft_authentication_example/MainActivity.kt
@@ -1,14 +1,8 @@
 package za.co.britehouse.flutter_microsoft_authentication_example
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+// MainActivity.kt
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 
 }
